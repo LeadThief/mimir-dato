b422ec2ef4bd694971c9e2de020adfada7cfea89
cadams <cadams@nutramaxlabs.com>
2020-11-06 14:34:56 -0500

    first commit

---
+++ components/post-header.js
@@ -0,0 +1,23 @@
+import Avatar from '../components/avatar'
+import Date from '../components/date'
+import CoverImage from '../components/cover-image'
+import PostTitle from '../components/post-title'
+
+export default function PostHeader({ title, coverImage, date, author }) {
+  return (
+    <>
+      <PostTitle>{title}</PostTitle>
+      <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
+        <CoverImage
+          title={title}
+          responsiveImage={coverImage.responsiveImage}
+        />
+      </div>
+      <div className="max-w-2xl mx-auto">
+        <div className="mb-6 text-lg">
+          <Date dateString={date} />
+        </div>
+      </div>
+    </>
+  )
+}
